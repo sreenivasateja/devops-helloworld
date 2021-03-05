7345d0e31399b8a7d01f79ace4caa2183e19c03b
sreenivasateja <79838676+sreenivasateja@users.noreply.github.com>
2021-03-05 15:17:49 +0530

    first commit

--- sample_code.sh
+++ sample_code.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #
 
-echo "This is an Samples Script from GitHub"
+echo "This is an Samples Script from GitHub webhook"
 echo "This is test from Linux Server"
 echo "This is an example of a script"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
+
